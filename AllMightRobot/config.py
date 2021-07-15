411e5abbdfda38b58269c33a30f80058286fc666
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-07-15 20:28:32 +0500

    Update config.py

--- AllMightRobot/config.py
+++ AllMightRobot/config.py
@@ -1,9 +1,5 @@
-# Copyright (C) 2018 - 2020 MrYacha. All rights reserved. Source code available under the AGPL.
-# Copyright (C) 2019 Aiogram
-# Copyright (C) 2019 pqhaz
-#
-# This file is part of AllMightBot.
-#
+# This file is part of Daisy (Telegram Bot)
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
@@ -18,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 
 import yaml
 from envparse import env
@@ -25,29 +22,27 @@ from envparse import env
 from AllMightRobot.utils.logger import log
 
 DEFAULTS = {
-    'LOAD_MODULES': True,
-    'DEBUG_MODE': True,
-
-    'REDIS_HOST': 'localhost',
-    'REDIS_PORT': 6379,
-    'REDIS_DB_FSM': 1,
-
-    'MONGODB_URI': 'localhost',
-    'MONGO_DB': 'AllMight',
-
-    'API_PORT': 8080,
-
-    'JOIN_CONFIRM_DURATION': '30m',
+    "LOAD_MODULES": True,
+    "DEBUG_MODE": True,
+    "REDIS_HOST": "localhost",
+    "REDIS_PORT": 6379,
+    "REDIS_DB_FSM": 1,
+    "MONGODB_URI": "localhost",
+    "MONGO_DB": "DaisyX",
+    "API_PORT": 8080,
+    "JOIN_CONFIRM_DURATION": "30m",
 }
 
-CONFIG_PATH = 'data/bot_conf.yaml'
-if os.name == 'nt':
+CONFIG_PATH = "data/bot_conf.yaml"
+if os.name == "nt":
     log.debug("Detected Windows, changing config path...")
     CONFIG_PATH = os.getcwd() + "\\data\\bot_conf.yaml"
 
 if os.path.isfile(CONFIG_PATH):
     log.info(CONFIG_PATH)
-    for item in (data := yaml.load(open('data/bot_conf.yaml', "r"), Loader=yaml.CLoader)):
+    for item in (
+        data := yaml.load(open("data/bot_conf.yaml", "r"), Loader=yaml.CLoader)
+    ):
         DEFAULTS[item.upper()] = data[item]
 else:
     log.info("Using env vars")
@@ -59,11 +54,11 @@ def get_str_key(name, required=False):
     else:
         default = None
     if not (data := env.str(name, default=default)) and not required:
-        log.warn('No str key: ' + name)
+        log.warn("No str key: " + name)
         return None
     elif not data:
-        log.critical('No str key: ' + name)
-        exit(2)
+        log.critical("No str key: " + name)
+        sys.exit(2)
     else:
         return data
 
@@ -74,11 +69,11 @@ def get_int_key(name, required=False):
     else:
         default = None
     if not (data := env.int(name, default=default)) and not required:
-        log.warn('No int key: ' + name)
+        log.warn("No int key: " + name)
         return None
     elif not data:
-        log.critical('No int key: ' + name)
-        exit(2)
+        log.critical("No int key: " + name)
+        sys.exit(2)
     else:
         return data
 
@@ -89,11 +84,11 @@ def get_list_key(name, required=False):
     else:
         default = None
     if not (data := env.list(name, default=default)) and not required:
-        log.warn('No list key: ' + name)
+        log.warn("No list key: " + name)
         return []
     elif not data:
-        log.critical('No list key: ' + name)
-        exit(2)
+        log.critical("No list key: " + name)
+        sys.exit(2)
     else:
         return data
 
@@ -104,10 +99,10 @@ def get_bool_key(name, required=False):
     else:
         default = None
     if not (data := env.bool(name, default=default)) and not required:
-        log.warn('No bool key: ' + name)
+        log.warn("No bool key: " + name)
         return False
     elif not data:
-        log.critical('No bool key: ' + name)
-        exit(2)
+        log.critical("No bool key: " + name)
+        sys.exit(2)
     else:
         return data
